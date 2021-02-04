ff43caee6e75529ca8a083308925e729a7d28533
Alexis <alex.p.flores17@gmail.com>
2021-02-04 03:15:37 -0600

    MainContent.js

--- src/Componentes/MainContent.js
+++ src/Componentes/MainContent.js
@@ -4,7 +4,18 @@ export const MainContent = () =>{
 
     return(
         <div className="main-container col-12">
-    
+            <div className="tituloMainContent col-9">
+                <h1>¿En qué ciudad te encuentras?</h1>
+                <p className="textoContent">
+                    Este es un listado de ciudades en donde está disponible 
+                    el servicio de Oxígeno México CC. Cada sitio esta administrado 
+                    por un equipo local , si te interesa implementarlo en tu ciudad 
+                    puedes contactarnos al correo <a className="hoverMain" href="mailto:oxigeno.cc.mx@gmail.com">oxigeno.cc.mx@gmail.com</a>
+                </p>
+            </div>
+            <div className="listadoMainContent" >
+                
+            </div>
         </div>
     );
 }
