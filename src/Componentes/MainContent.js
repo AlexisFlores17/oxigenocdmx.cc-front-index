c103af9a40ff219d2d6c4c369a4903a549290d9d
Alexis <alex.p.flores17@gmail.com>
2021-02-04 21:06:45 -0600

    PrimerSprint

--- src/Componentes/MainContent.js
+++ src/Componentes/MainContent.js
@@ -5,7 +5,7 @@ export const MainContent = () =>{
     return(
         <div className="main-container col-12">
             <div className="tituloMainContent col-xs-10 col-md-9">
-                <h1>¿En qué ciudad te encuentras?</h1>
+                <h1 className="tituloContent" >¿En qué ciudad te encuentras?</h1>
                 <p className="textoContent">
                     Este es un listado de ciudades en donde está disponible 
                     el servicio de Oxígeno México CC. Cada sitio está administrado 
@@ -14,10 +14,13 @@ export const MainContent = () =>{
                 </p>
             </div>
             <div className="listadoMainContent" >
-                <div className="buttonContainer col-4">
+                <div className="buttonContainer col-xs-12 col-sm-6 col-md-4">
                     <a href="https://oxigenocdmx.cc"><div className="buttonMainContent">CDMX</div></a>
                 </div>
-                <div className="buttonContainer col-4">
+                <div className="buttonContainer  col-xs-12 col-sm-6 col-md-4">
+                    <a href="https://oxigenocdmx.cc"><div className="buttonMainContent">Próximamente</div></a>
+                </div>
+                <div className="buttonContainer  col-xs-12 col-sm-6 col-md-4">
                     <a href="https://oxigenocdmx.cc"><div className="buttonMainContent">Próximamente</div></a>
                 </div>
             </div>
